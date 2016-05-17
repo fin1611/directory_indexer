3b5019eb1c07d743ce650995f71a5d25fb9754a6
Fin <fin@breitmann.com>
2016-05-17 22:05:54 +0200

    works

--- src/DirectoryIndexer.java
+++ src/DirectoryIndexer.java
@@ -1,9 +1,65 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DirectoryIndexer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static PrintWriter pw;
+	
+	public static void main(String[] args) 
+		throws Exception
+	{
+		String path = args[0];
+		
+		System.out.println("Path: " + path);
+		
+		File resultFile = new File( new File(path), "index.txt");
+		pw = new PrintWriter(new FileWriter(resultFile));
+		
+		scanDir( new File( path ) );
+		
+		pw.close();
+		
+		System.out.println("Finished");
+	}
+	
+	private static List<String> scanDir( File dir )
+			throws Exception
+	{
+		System.out.println(dir);
+		
+		List<String> fileNames = new ArrayList<String>();
+		
+		File[] files = dir.listFiles();
+		for( File file : files )
+		{
+			if( file.getName().startsWith( ".") ) continue;
+			
+			if( file.isDirectory() )
+			{
+				List<String> subDirFileNames = scanDir( file );
+				
+				if( subDirFileNames.size() > 0 )
+				{
+					pw.println( file );
+					
+					for( String subFileName : subDirFileNames )
+					{
+						pw.println( subFileName );
+					}
+					
+					pw.println();
+				}
+			}
+			else
+			{
+				fileNames.add( file.getName() );
+			}
+		}
+		
+		return fileNames;
 	}
 
 }
